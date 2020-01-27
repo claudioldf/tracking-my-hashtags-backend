fe6a0c442f6691c505ca1cef304446adfff83931
claudioldf <claudioldf@gmail.com>
2020-01-26 21:28:03 -0300

    Add new test to Hashtag#name

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,4 +1,10 @@
 class Hashtag < ApplicationRecord
   # Validations
   validates_presence_of :name
+
+  def name
+    value = super
+    value = "#" + value.gsub('#', '') if value.present?
+    value
+  end
 end
--- spec/models/hashtag_spec.rb
+++ spec/models/hashtag_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe Hashtag, type: :model do
 
   describe "#name" do
+    it "should return '#' at the beginnig of a name" do
+      hashtag = Hashtag.new
+      hashtag.name = "ruby"
+      expect(hashtag.name).to eq("#ruby")
+    end
+
     it "should not save without name" do
       hashtag = Hashtag.new
       expect(hashtag.save).to eq(false)
